6efc37bcad17a6f0d9879c48deac5b47624a88c4
Lars Peter Jørgensen <lars-games@outlook.com>
2022-09-12 15:25:55 +0200

    .

--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -11,8 +11,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 //Todo Remove or change relevant parts before ACTUAL use
-@Path("xxx")
-public class RenameMeResource {
+@Path("person")
+public class PersonResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
        
@@ -24,21 +24,20 @@ public class RenameMeResource {
     public String demo() {
         return "{\"msg\":\"Hello World\"}";
     }
-    @Path("count")
+    
     @GET
+    @Path("/all")
     @Produces({MediaType.APPLICATION_JSON})
-    public String getRenameMeCount() {
-       
-        long count = FACADE.getRenameMeCount();
-        //System.out.println("--------------->"+count);
-        return "{\"count\":"+count+"}";  //Done manually so no need for a DTO
+    public Response getAllPersons() {
+        return Response.ok().entity(GSON.toJson(FACADE.getAll())).build();
     }
-    @POST
-    @Produces({MediaType.APPLICATION_JSON})
-    @Consumes({MediaType.APPLICATION_JSON})
-    public Response postExample(String input){
-        RenameMeDTO rmdto = GSON.fromJson(input, RenameMeDTO.class);
-        System.out.println(rmdto);
-        return Response.ok().entity(rmdto).build();
-    }
+    
+//    @POST
+//    @Produces({MediaType.APPLICATION_JSON})
+//    @Consumes({MediaType.APPLICATION_JSON})
+//    public Response postExample(String input){
+////        RenameMeDTO rmdto = GSON.fromJson(input, RenameMeDTO.class);
+////        System.out.println(rmdto);
+//        return Response.ok().entity().build();
+//    }
 }
